fa70ff1cb7bd2fd54cd0c9c503dfb342c7ad91b7
m4tchl0ck <m4tchl0ck@outlook.com>
2024-10-24 13:37:03 -0700

    Update install-3-docker.sh

--- macOS/install-3-docker.sh
+++ macOS/install-3-docker.sh
@@ -1,4 +1,17 @@
 #/bin/sh
 ./update.sh
 
-brew install docker
+# brew install docker
+brew install podman
+
+podman machine init
+podman machine start
+
+sudo /opt/homebrew/Cellar/podman/5.2.5/bin/podman-mac-helper install
+podman machine stop; podman machine start
+        
+# sudo touch /usr/local/bin/docker
+# sudo bash -c 'echo "#!/bin/bash
+# exec podman \"\$@\"" > /usr/local/bin/docker'
+
+# sudo chmod +x /usr/local/bin/docker
