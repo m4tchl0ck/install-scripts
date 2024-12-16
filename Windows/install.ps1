ec39c45ea72ca42188ebdd0964b7c9b4a31e59ea
m4tchl0ck <m4tchl0ck@outlook.com>
2024-12-16 17:03:48 +0100

    Create windows/install.ps1

---
+++ Windows/install.ps1
@@ -0,0 +1,19 @@
+winget install --id=twpayne.chezmoi  -e
+winget install github-cli
+gh auth setup-git
+winget install vscode
+winget install --id=direnv.direnv  -e
+[Environment]::SetEnvironmentVariable("DIRENV_CONFIG", "$env:APPDATA\direnv\conf", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("XDG_CACHE_HOME", "$env:APPDATA\direnv\cache", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("XDG_DATA_HOME", "$env:APPDATA\direnv\data", [System.EnvironmentVariableTarget]::User)
+
+winget install podman
+podman machine init
+podman machine start
+# for compatibility with docker
+echo "Set-Alias -Name docker -Value podman -Scope Global" >> $profile
+
+winget install RedHat.Podman-Desktop
+
+# Go to  Settings > Resources.
+# In the Compose tile, click Setup, and follow the prompts.
