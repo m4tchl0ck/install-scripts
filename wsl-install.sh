4f403d822c12e97b2a8cc5c7d210e60e0493af03
m4tchl0ck <m4tchl0ck@outlook.com>
2025-04-04 23:12:55 +0200

    Update wsl-install.sh

--- wsl-install.sh
+++ wsl-install.sh
@@ -4,3 +4,17 @@
 sudo add-apt-repository ppa:wslutilities/wslu
 sudo apt update
 sudo apt install wslu
+
+
+# Podman
+# https://github.com/containers/podman/releases/latest
+wget https://github.com/containers/podman/releases/download/v4.9.1/podman-remote-static-linux_amd64.tar.gz
+sudo tar -C /usr/local -xzf podman-remote-static-linux_amd64.tar.gz
+export PATH="$PATH:/usr/local/bin"
+alias podman='podman-remote-static-linux_amd64'
+podman system connection add --default podman-machine-default-root unix:///mnt/wsl/podman-sockets/podman-machine-default/podman-root.sock
+# rootless
+podman system connection add --default podman-machine-default-user unix:///mnt/wsl/podman-sockets/podman-machine-default/podman-user.sock
+sudo usermod --append --groups 10 $(whoami)
+#check
+podman ps
