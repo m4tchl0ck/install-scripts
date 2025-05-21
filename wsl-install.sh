962ac6d120dffdbb679d1cff501215051c09e951
m4tchl0ck <m4tchl0ck@outlook.com>
2025-05-21 09:31:23 +0200

    Update wsl-install.sh

--- wsl-install.sh
+++ wsl-install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+echo '##################'
+echo '# Install gh-cli #'
+echo '##################'
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install -y gh
+
 #cp ~/.giconfig
 sudo add-apt-repository ppa:wslutilities/wslu
 sudo apt update
